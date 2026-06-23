d8801d626393db1eb00c1f07c233fac6f3d21f61
ghassan fashion <ghassanfashion8@gmail.com>
2026-06-23 21:04:18 +0000

    File changes

--- src/components/MedcorpsLogo.jsx
+++ src/components/MedcorpsLogo.jsx
@@ -4,12 +4,11 @@ const LOGO_URL = 'https://media.base44.com/images/public/6a2feb0cde2056469cbfde9
 
 export default function MedcorpsLogo({ className = '' }) {
   return (
-    <div className={`inline-flex rounded-xl overflow-hidden ${className}`}>
-      <img
-        src={LOGO_URL}
-        alt="Medcorps Espaço da Saúde"
-        className="h-full w-auto"
-      />
-    </div>
+    <img
+      src={LOGO_URL}
+      alt="Medcorps Espaço da Saúde"
+      className={`rounded-xl ${className}`}
+      style={{ imageRendering: 'auto' }}
+    />
   );
 }
